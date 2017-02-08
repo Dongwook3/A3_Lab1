0a20b16d74e006dee8419cb08630ce5f655de670
Dongwook3 <d_shin2@fanshaweonline.ca>
2017-02-08 12:40:02 -0500

    Add files via upload
    modified mainJQ.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 (function() {
 	var carImages = document.querySelectorAll('.thumbInfo img'),
-	modelName = document.querySelector('.modelName'),
-	priceInfo = document.querySelector('.priceInfo'),
-	modelDetails = document.querySelector('.modelDetails'),
+		modelName = document.querySelector('.modelName'),
+		priceInfo = document.querySelector('.priceInfo'),
+		modelDetails = document.querySelector('.modelDetails'),
 
 	function makeRequest() {
 		httpRequest = new XMLHttpRequest();
@@ -17,18 +17,13 @@
 	}
 
 	function showCarInfo() {
-		if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200)
+		if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
 			model = JSON.parse(httpRequest.responseText);
-
-			modelName.firstChild.nodeValue = modelName;
-
-			[].forEach.call(document.querySelectorAll('.hidden'), function(item){
-				item.classList.remove('hidden');
-			})
 
 			modelName.firstChild.nodeValue = model.modelName;
 			priceInfo.firstChild.nodeValue = model.pricing;
 			modelDetails.firstChild.nodeValue = model.modelDetails;
+		}
 	}
 
 	[].forEach.call(carImages, function(el) {
--- js/mainJQ.js
+++ js/mainJQ.js
@@ -2,9 +2,7 @@
 	$('.thumbInfo img').on('click', function () {
 		//add JSON call here
 		$.getJSON('includes/ajaxQuery.php', { model:this.id }, function(data) {
-			console.log(data);
-			//$('.thumbInfo img').text(mainmodel.modelName);
-			//$('.hidden').removeClass('hidden');
+			//console.log(data);
 
 			$('.modelName').text(data.modelName)
 			$('.priceInfo').text(data.pricing)
