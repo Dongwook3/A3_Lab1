641254a786de633a4f2f9728495122a61550a9ae
Dongwook3 <d_shin2@fanshaweonline.ca>
2017-02-08 12:52:32 -0500

    Add files via upload
    modified mainJQ.js but I don't know why 'main.js' doesn't work.

--- js/mainJQ.js
+++ js/mainJQ.js
@@ -3,7 +3,7 @@
 		//add JSON call here
 		$.getJSON('includes/ajaxQuery.php', { model:this.id }, function(data) {
 			//console.log(data);
-
+			$('.subhead span').text(data.modelName)
 			$('.modelName').text(data.modelName)
 			$('.priceInfo').text(data.pricing)
 			$('.modelDetails').text(data.modelDetails);
